f4a6f71f7474bba7649ae68d4bad73f4bac6f4b5
DEIVIDTEIXXEIRA <deividteixeira.go@gmail.com>
2023-08-17 13:09:05 -0300

    função que verifica se as rotas da API estão sendo roteadas.

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,29 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func Saude(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
+	msgJson, erro := json.Marshal(
+		map[string]string{"Saúde da API": "OK"},
+	)
+
+	if erro != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("Error"))
+		fmt.Printf("Error controllers.Saude -> %s\n", erro)
+	}
+
+	w.WriteHeader(200)
+	w.Write(msgJson)
+	fmt.Printf("package controllers.Saude -> %s\n", "OK")
+
+
 }
 func Autorizacao(w http.ResponseWriter, r *http.Request)              {}
 func TodosDisponiveis(w http.ResponseWriter, r *http.Request)         {}
